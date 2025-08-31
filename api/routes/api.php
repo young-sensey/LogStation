d87ca264503b3bb544c11a92135281ea78f0f1c5
Airat <young.sensey.94@gmail.com>
2025-08-31 16:16:24 +0400

    Add basic authorization to the route to get device logs.

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DeviceLogController;
+use App\Http\Middleware\AuthenticateOnceWithBasicAuth;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(DeviceLogController::class)->group(function () {
-    Route::get('/logs/view', 'view');
+    Route::get('/logs/view', 'view')->middleware(AuthenticateOnceWithBasicAuth::class);
     Route::put('/logs/upload', 'upload')->middleware('auth:sanctum');
 });
 
